db9b3a69dd048bd85e87c7de3070f65b4c13fa24
Muhammadinaam <minaammunir@gmail.com>
2018-03-23 01:36:07 +0500

    hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './adminlte/header/header.component';
+import { SidebarComponent } from './adminlte/sidebar/sidebar.component';
+import { FooterComponent } from './adminlte/footer/footer.component';
+import { ContentComponent } from './adminlte/content/content.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
