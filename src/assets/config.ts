d52856583a08317a95e52b5a926318aaea1433c1
Muhammadinaam <minaammunir@gmail.com>
2018-04-17 00:45:50 +0500

    hello

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,7 +1,7 @@
 export const config = {
     
-    //base_url: "http://192.168.100.7/rms/public",
-    base_url: "http://localhost/rms/public",
+    base_url: "http://192.168.100.7/rms/public",
+    //base_url: "http://localhost/rms/public",
 
     auth_data : {
         grant_type: 'password',
